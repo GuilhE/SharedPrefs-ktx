48e8b8a21f0701dc20c3553e6f5ebca0d755729a
GuilhE <gdelgado@bliss.pt>
2020-10-06 15:09:35 +0100

    Update prepare_mkdocs.sh

--- .github/workflows/prepare_mkdocs.sh
+++ .github/workflows/prepare_mkdocs.sh
@@ -40,10 +40,10 @@ mkdir docs/usage/
 cp converter-gson/README.md docs/usage/converter-gson.md
 cp converter-moshi/README.md docs/usage/converter-moshi.md
 
-O1="./converter-gson"
-R1="./usage/converter-gson"
-O2="./converter-moshi"
-R2="./usage/converter-moshi"
+O1="converter-gson"
+R1="usage/converter-gson"
+O2="converter-moshi"
+R2="usage/converter-moshi"
 
 sed -i "" "s/${O1}/${R1}/g" docs/index.md
 sed -i "" "s/${O2}/${R2}/g" docs/index.md
