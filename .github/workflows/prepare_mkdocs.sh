703075360d1bd2f080b987622919fb6468d225c9
GuilhE <gdelgado@bliss.pt>
2020-10-06 15:27:56 +0100

    Update prepare_mkdocs.sh

--- .github/workflows/prepare_mkdocs.sh
+++ .github/workflows/prepare_mkdocs.sh
@@ -33,10 +33,10 @@ set -x
 
 # Copy in special files that GitHub wants in the project root.
 
-O1="<a href='./converter-gson'>"
-R1="<a href='./usage/converter-gson'>"
-O2="<a href='./converter-moshi'>"
-R2="<a href='./usage/converter-moshi'>"
+O1="./converter-gson"
+R1="./usage/converter-gson"
+O2="./converter-moshi"
+R2="./usage/converter-moshi"
 
 sed -i "" "s/${O1}/${R1}/g" README.md
 sed -i "" "s/${O2}/${R2}/g" README.md
