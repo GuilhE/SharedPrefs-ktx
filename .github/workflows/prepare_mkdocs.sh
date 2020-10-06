6bf2f3ec9e43bb732a7d8883fb5702619739e191
GuilhE <gdelgado@bliss.pt>
2020-10-06 12:48:47 +0100

    Update prepare_mkdocs.sh

--- .github/workflows/prepare_mkdocs.sh
+++ .github/workflows/prepare_mkdocs.sh
@@ -36,6 +36,6 @@ cat README.md | grep -v 'project website' > docs/index.md
 cp CHANGELOG.md docs/changelog.md
 cp CONTRIBUTING.md docs/contributing.md
 cp CODE_OF_CONDUCT.md docs/conduct.md
-cp converter-gson/README.md docs/conveter-gson.md
-cp converter-moshi/README.md docs/conveter-moshi.md
+cp converter-gson/README.md docs/converter-gson.md
+cp converter-moshi/README.md docs/converter-moshi.md
 #cp -r media docs/media
